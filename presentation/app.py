a0882c76d22841fac575e919760e2ffd6bbad8cb
Faris Chebib <faris@zaneprep.com>
2014-11-14 12:21:43 -0700

    added dyanmic db

--- presentation/app.py
+++ presentation/app.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 
 from flask import Flask, render_template, url_for
+
+from db import db
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,6 +13,7 @@ def index():
     ctx['author'] = "Faris Chebib"
     template_name = "index.html"
     ctx['impress'] = url_for('static', filename='js/impress.js')
+    ctx['slides'] = db['slides']
     return render_template(template_name, **ctx)
 
 if __name__ == "__main__":
---
+++ presentation/db.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+db = {
+    'slides': [
+        {
+        'id': 'prestitle',
+        'animation': 'slide',
+        'x': '-1000',
+        'y': '-1500',
+        'scale': 1,
+        'content': """
+            <q>Aren't you just <b>bored</b> with all those slides-based presentations?</q>
+        """
+        },
+        {
+        'id': '',
+        'animation': 'slide',
+        'x': '0',
+        'y': '-1500',
+        'scale': 1,
+        'content': """
+            <q>Don't you think that presentations given <strong>in modern browsers</strong> shouldn't <strong>copy the limits</strong> of 'classic' slide decks?</q>
+        """
+        },
+    ]
+}
