dd0e7de0052c2ffb4752e8592f46c1d9df9d5188
Faris Chebib <faris@zaneprep.com>
2014-12-10 15:17:57 -0700

    added lsystems refs

--- presentation/db.py
+++ presentation/db.py
@@ -1,23 +1,37 @@
-# encoding: utf-8
-
 from utils import render_code
 
 db = {
     'slides': [
         {
-        'id': 'home',
-        'animation': 'slide',
-        'x': '-1000',
-        'y': '-1500',
-        'z': '-150',
-        'rotate': '50',
-        'rotate_x': '-10',
-        'rotate_y': '-40',
-        'scale': '10',
-        'content': """
-            <q>Aren’t you just <b>bored</b> with all those slides-based presentations?</q>
-            {code}
-        """.format(code=render_code('[x.first_name, x.last_name for x in people]'))
+          'id': 'title',
+          'animation': 'slide',
+          'x': '0',
+          'y': '0',
+          'z': '0',
+          'rotate': '0',
+          'rotate_x': '0',
+          'rotate_y': '0',
+          'scale': 0,
+          'content': """
+          <h1>The Art of Python</h1>
+          <h2>Faris Chebib</h2>
+          <h3>2014-12-10</h3>
+          """
+        },
+        {
+          'id': 'art',
+          'animation': 'slide',
+          'x': '0',
+          'y': '1000',
+          'z': '-500',
+          'rotate': '0',
+          'rotate_x': '180',
+          'rotate_y': '180',
+          'scale': 0,
+          'content': """
+                <q>Aren’t you just <b>bored</b> with all those slides-based presentations?</q>
+                {code}
+          """.format(code=render_code('[x.first_name, x.last_name for x in people]'))
         },
         {
         'id': '',
@@ -32,5 +46,16 @@ db = {
             shouldn’t <strong>copy the limits</strong> of ‘classic’ slide decks?</q>
         """
         },
+        {
+          'id': 'overview',
+          'animation': 'slide',
+          'x': '0',
+          'y': '0',
+          'z': '0',
+          'rotate': '0',
+          'rotate_x': '0',
+          'rotate_y': '0',
+          'scale': 0
+        }
     ]
 }
